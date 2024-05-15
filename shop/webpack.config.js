5de6fdb409c40f2360a360ebe3b931410b08c19c
Lily <116434991+LilithyTv@users.noreply.github.com>
2024-05-15 13:40:37 +0200

    Add files via upload

---
+++ shop/webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Your entry point
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js' // Output file name
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Transpile JavaScript and JSX files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'] // Use Babel presets for JS and React
+          }
+        }
+      },
+      {
+        test: /\.css$/, // Process CSS files
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html' // Reference template for HTML
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'] // Resolve these file types
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+    open: true,
+    historyApiFallback: true
+  }
+};
